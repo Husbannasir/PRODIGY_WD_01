63310cc5865ea7047abc92676cafbe1852e91c89
Husbannasir <144462158+Husbannasir@users.noreply.github.com>
2023-09-08 19:29:43 +0500

    Add files via upload

---
+++ internship 1.js
@@ -0,0 +1,9 @@
+// JavaScript to add scrolling and color change effects
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
